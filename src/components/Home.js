165c1b42b584d19a8dc0cf6e004ea247994f7a9c
Madi Baqyt <bakyt.madi.work@gmail.com>
2021-04-30 16:45:26 +0300

    Add header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,34 @@
 import React from "react";
+import { Navbar, Nav, Container, Dropdown } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
-    return(
-        <h1>Hello world!</h1>
+    return (
+        <div className="home-container">
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container fluid="md">
+                    <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link href="/poets">Poets</Nav.Link>
+                            <Nav.Link href="#desc">Description</Nav.Link>
+                            <Nav.Link href="#daily">Daily</Nav.Link>
+                            <Nav.Link href="#devs">Developers</Nav.Link>
+                            <Dropdown>
+                                <Dropdown.Toggle>
+                                    Language
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item>Russian</Dropdown.Item>
+                                    <Dropdown.Item>English</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
     );
 }
 
